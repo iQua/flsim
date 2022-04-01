f07e7a6ead833dfbf7bda9c1dd73d6fa4ecf0064
Orienfish <x1yu@ucsd.edu>
2022-04-01 15:59:22 -0700

    update FEMNIST model

--- models/FEMNIST/fl_model.py
+++ models/FEMNIST/fl_model.py
@@ -11,6 +11,7 @@ import json
 import os
 
 IMAGE_SIZE = 28
+NUM_CLASSES = 62
 
 # Training settings
 lr = 0.01
@@ -69,17 +70,17 @@ class Generator(load_data.Generator):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4 * 4 * 50, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.conv1 = nn.Conv2d(1, 32, 5, 1, padding='same')
+        self.conv2 = nn.Conv2d(32, 64, 5, 1, padding='same')
+        self.fc1 = nn.Linear(7 * 7 * 64, 2048)
+        self.fc2 = nn.Linear(2048, NUM_CLASSES)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4 * 4 * 50)
+        x = x.view(-1, 7 * 7 * 64)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
